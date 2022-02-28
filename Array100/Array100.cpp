28a4b7216a49964cc144e425663fa152017fb675
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-02-28 22:38:13 +0300

    110recursion

--- Array100/Array100.cpp
+++ Array100/Array100.cpp
@@ -6,8 +6,71 @@
 
 #include <iostream>
 using namespace std;
+int p;
+int min10Arr(int a[100], int n) {
+    int sum = 0;
+    for (int i = n; i < n + 10; ++i)
+        sum += a[i];
+    //cout << "n: " << n << "; s: " << sum << endl;
+    if (100 - n == 10)
+    {
+        ::p = n;
+        return sum;
+    }
+    else
+    {
+        int sum1 = min10Arr(a, n + 1);
+        if (sum <= sum1)
+        {
+            ::p = n;
+            return sum;
+        }
+        else return sum1;
+    }
+}
 
-int min10Arr(int arr[100], int n = 0)
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    const int size = 100;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 50;
+        cout << arr[i] << "\t";
+        if ((i + 1) % 10 == 0)
+            cout << endl;
+    }
+    cout << endl;
+    cout << "Минимальная сумма - " << min10Arr(arr, 0);
+    cout << "\nПозиция - " << ::p;
+    cout << endl;
+    cout << endl;
+
+    //проверка
+    int sum1 = 0, sum2 = 0, p2;
+    for (int i = 0; i < 90; i++)
+    {
+        sum1 = 0;
+        for (int j = i; j < i + 10; j++)
+        {
+            sum1 += arr[j];            
+        }
+        if (sum1 < sum2 || i == 0)
+        {
+            sum2 = sum1;
+            p2 = i;
+        }
+        //cout << sum1 << "\t"<< i << "\n";
+    }
+    cout << "Проверка - " << sum2 << "\t" << p2;
+    return 0;
+}
+
+
+
+/*int min10Arr(int arr[100], int n = 0)
 {
     if (n == 89)
     {
@@ -24,95 +87,5 @@ int min10Arr(int arr[100], int n = 0)
         p = n;
         return p;
     }
-    return n;    
-}
-
-int main()
-{
-    srand(time(NULL));
-    const int size = 100;
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rand() % 50;
-        cout << arr[i] << "\t";
-        if ((i + 1) % 10 == 0)
-            cout << endl;
-    }
-    cout << endl;
-    cout << min10Arr(arr);
-    cout << endl;
-    cout << endl;
-
-    //проверка
-    int sum1 = 0, sum2 = 0, p;
-    for (int i = 0; i < 90; i++)
-    {
-        sum1 = 0;
-        for (int j = i; j < i + 10; j++)
-        {
-            sum1 += arr[j];            
-        }
-        if (sum1 < sum2 || i == 0)
-        {
-            sum2 = sum1;
-            p = i;
-        }
-        cout << sum1 << "\t"<< i << "\n";
-    }
-    cout << p << "\t" << sum2;
-    return 0;
-}
-
-/*(int a[100], int n, int& p) {
-    int s = 0;
-    for (int i = n; i < n + 10; ++i) s += a[i];
-    cout << "n: " << n << "; s: " << s << endl;
-    if (100 - n == 10)
-    {
-        p = n;
-        return s;
-    }
-    else
-    {
-        int s1 = min10(a, n + 1, p);
-        if (s <= s1)
-        {
-            p = n;
-            return s;
-        }
-        else return s1;
-    }
-}
-int main() {
-    int a[100];
-    int p;
-
-    
-
-    for (int i = 0; i < 100; ++i) a[i] = rnd​(​generator​)​;
-    for (int i = 0; i < 100; ++i) cout << a[i] << '\t';
-    cout << endl << endl;
-
-    min10(a, 0, p);
-
-    cout << "Minimum position: " << p << endl << endl;
-
-    system​(​"pause"​)​; ​
-}
-
-int min10(int a[100], bestIndex = 0, bestSum = 0, currentSum = 0, currentIndex = 0) {
-if (currentIndex == 100)
-return bestIndex;
-
-int newSum = currentSum + a[currentIndex];
-
-if (currentIndex < 10)
-return min10(a, bestIndex, newSum, newSum, currentIndex + 1);
-
-newSum = newSum - a[currentIndex - 10];
-if (newSum > bestSum)
-return min10(a, currentIndex - 10, newSum, newSum, currentIndex + 1);
-else
-return min10(a, bestIndex, bestSum, newSum, currentIndex + 1);
+    return n;
 }*/
